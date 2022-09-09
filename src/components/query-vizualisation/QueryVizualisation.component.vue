8bc1f17b508909ae39d04038860706c276e495e7
keithCuniah <88380932+keithCuniah@users.noreply.github.com>
2022-09-09 04:42:25 +0200

    Vizualisation component

---
+++ src/components/query-vizualisation/QueryVizualisation.component.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-card class="my-card">
+    <q-card-section class="bg-primary text-white">
+      <div class="text-h6">Query :</div>
+    </q-card-section>
+
+    <span class="query-area">
+      <slot></slot>
+    </span>
+    <q-card-actions align="around">
+      <q-btn flat @click="$emit('onSubmit')">Submit</q-btn>
+    </q-card-actions>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: "QueryVizualisationComponent",
+};
+</script>
+
+<style lang="scss" scoped></style>
